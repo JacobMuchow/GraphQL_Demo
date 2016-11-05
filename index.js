34109bba1df6d9a6885edef47e708045a0474631
Jacob Muchow <jacobamuchow@gmail.com>
2016-11-04 19:12:16 -0500

    Setup demo GraphQL server.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 var express = require('express');
-var app = express();
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
 
-app.set('port', (process.env.PORT || 5000));
+var schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+var root = { 
+    hello: () => {
+        return 'Hello world!';
+    }
+};
+
+var app = express();
+app.set('port', (4000));
 
 //app.use(express.static(__dirname + '/public'));
 
@@ -12,6 +26,12 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('index');
 });
+
+app.get('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
