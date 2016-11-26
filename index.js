61d7d28ed14e9731e46aa8f64e71674d43b02cf5
Jacob Muchow <jacobamuchow@gmail.com>
2016-11-26 12:14:19 -0600

    Query using GraphQL on client.

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.set('port', (process.env.PORT || 4000));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+//Client-side modules
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
 require ('./api/rest/routes.js')(app);
 require ('./api/graphql/routes.js')(app);
 
