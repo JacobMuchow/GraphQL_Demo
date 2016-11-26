424d99be440cfca482fb3b33a5f494c091426fe0
Jacob Muchow <jacobamuchow@gmail.com>
2016-11-26 11:23:37 -0600

    Learned promises for GraphQL, started db for REST API.

--- api/graphql/routes.js
+++ api/graphql/routes.js
@@ -1,23 +1,84 @@
 var pg = require('pg');
 var graphqlExp = require('express-graphql');
-var { buildSchema } = require('graphql');
 
-var schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`);
+var {
+  graphql,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema
+} = require('graphql');
 
-var root = { 
-    hello: () => {
-        return 'Hello world!';
+var TODOs = [  
+  {
+    "id": 1446412739542,
+    "title": "Read emails",
+    "completed": false
+  },
+  {
+    "id": 1446412740883,
+    "title": "Buy orange",
+    "completed": true
+  }
+];
+
+let TodoType = new GraphQLObjectType({
+    name: 'todo',
+    fields: function() {
+        return {
+            id: {
+                type: GraphQLID
+            },
+            title: {
+                type: GraphQLString
+            },
+            completed: {
+                type: GraphQLBoolean
+            }
+        }
     }
-};
+});
+
+let QueryType = new GraphQLObjectType({
+    name: 'Query',
+    fields: function() {
+        return {
+            todos: {
+                type: new GraphQLList(TodoType),
+                resolve: function() {
+                    return new Promise(function(resolve, reject) {
+
+                        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+                            resolve(TODOs);
+                            // client.query('SELECT * FROM test_table', function(err, result) {
+                            //     done();
+                            //     if (err) {
+                            //         console.error(err);
+                            //         res.send("Error " + err);
+                            //     } else {
+                            //         res.json({
+                            //             result: result.rows
+                            //         });
+                            //     }
+                            // });
+                        });
+                    });
+                }
+            }
+        }
+    }
+});
+
+let Schema = new GraphQLSchema({
+    query: QueryType
+});
 
 module.exports = function(app) {
-    app.all('/api/graphql', graphqlExp({
-        schema: schema,
-        rootValue: root,
-        graphiql: false
+    app.use('/api/graphql', graphqlExp({
+        schema: Schema
     }));
 };
--- api/rest/routes.js
+++ api/rest/routes.js
@@ -1,21 +1,29 @@
 var pg = require('pg');
 
+var pool = new pg.Pool({
+    host: process.env.DATABASE_URL,
+    database: 'rootsnblues',
+    max: 10,
+    idleTimeoutMillis: 1000
+});
+
+pool.on('error', function(err, client) {
+    console.log(err);
+});
+
 module.exports = function(app) {
     app.get('/api/rest/test', test);
 };
 
 function test(req, res) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM test_table', function(err, result) {
-            done();
-            if (err) {
-                console.error(err);
-                res.send("Error " + err);
-            } else {
-                res.json({
-                    result: result.rows
-                });
-            }
-        });
+    pool.query('SELECT * FROM event', function(err, result) {
+        if (err) {
+            console.error(err);
+            res.send("Error " + err);
+        } else {
+            res.json({
+                result: result.rows
+            });
+        }
     });
 }
