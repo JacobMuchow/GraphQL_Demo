da5b0d0991b8a30e89992315d58f5f0714e8c91d
Jacob Muchos <jacobamuchow@gmail.com>
2016-12-06 00:50:11 -0600

    Updated seed data to keep serial sequence so as not to cause errors on first inserts.

--- seed_data.sql
+++ seed_data.sql
@@ -22,13 +22,13 @@ CREATE TABLE event (
     event_time timestamp
 );
 
-INSERT INTO artist (id, name, description) VALUES
-    (1, 'Buddy Guy', 'At age 65, he''s a Rock and Roll Hall of Fame inductee, a...'),
-    (2, 'Lucinda Williams', 'As a rule, you can divide music into three categories...');
+INSERT INTO artist (name, description) VALUES
+    ('Buddy Guy', 'At age 65, he''s a Rock and Roll Hall of Fame inductee, a...'),
+    ('Lucinda Williams', 'As a rule, you can divide music into three categories...');
 
-INSERT INTO festival (id, name, description) VALUES
-    (1, 'Roots N Blues N BBQ Festival', 'Annual Roots N Blues festival in Columbia, MO');
+INSERT INTO festival (name, description) VALUES
+    ('Roots N Blues N BBQ Festival', 'Annual Roots N Blues festival in Columbia, MO');
 
-INSERT INTO event (id, festivalId, artistId, event_time) VALUES
-    (1, 1, 1, '2015-09-25 21:30:00'),
-    (2, 1, 2, '2015-09-25 21:30:00');
+INSERT INTO event (festivalId, artistId, event_time) VALUES
+    (1, 1, '2015-09-25 21:30:00'),
+    (1, 2, '2015-09-25 21:30:00');
